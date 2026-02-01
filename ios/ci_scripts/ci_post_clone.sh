fafb19ee4c003b6e553d3a03723224bad4fcc95c
Matteo Carlone <m.carlone@reply.it>
2026-02-01 18:23:33 +0100

    fix flutter install

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,10 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-cd "$CI_WORKSPACE"
+# --- CALCOLO DINAMICO DELLA ROOT ---
+FLUTTER_ROOT_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
 
+cd "$FLUTTER_ROOT_DIR"
+
+# --- 1. INSTALLAZIONE FLUTTER ---
+rm -rf flutter
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable flutter
+export PATH="$FLUTTER_ROOT_DIR/flutter/bin:$PATH"
+
+flutter --version
+flutter precache --ios
+
+# --- 2. DIPENDENZE DART ---
 flutter pub get
-flutter build ios --config-only
 
+# --- 3. DIPENDENZE IOS ---
 cd ios
-pod install --repo-update
+rm -rf Pods Podfile.lock .symlinks
+pod install --verbose
