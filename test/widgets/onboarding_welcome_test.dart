e80dfbabd160def8c588e22447ff890fc6d85d45
mcarlone <mcarlone@cyberwave.com>
2026-07-12 10:49:30 +0200

    test: mock Supabase in onboarding welcome

--- test/widgets/onboarding_welcome_test.dart
+++ test/widgets/onboarding_welcome_test.dart
@@ -1,20 +1,35 @@
 import 'package:artrosi_cane/core/providers/shared_prefs_provider.dart';
+import 'package:artrosi_cane/core/providers/supabase_provider.dart';
 import 'package:artrosi_cane/features/onboarding/presentation/screens/onboarding_welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class _MockSupabaseClient extends Mock implements SupabaseClient {}
+
+class _MockGoTrueClient extends Mock implements GoTrueClient {}
 
 void main() {
   testWidgets('Render OnboardingWelcome with the new entry-page copy', (
     tester,
   ) async {
-    SharedPreferences.setMockInitialValues({});
+    SharedPreferences.setMockInitialValues({'invite_location': 'bibione'});
     final prefs = await SharedPreferences.getInstance();
+    final supabaseClient = _MockSupabaseClient();
+    final authClient = _MockGoTrueClient();
+
+    when(() => supabaseClient.auth).thenReturn(authClient);
+    when(() => authClient.currentSession).thenReturn(null);
 
     await tester.pumpWidget(
       ProviderScope(
-        overrides: [sharedPreferencesProvider.overrideWithValue(prefs)],
+        overrides: [
+          sharedPreferencesProvider.overrideWithValue(prefs),
+          supabaseClientProvider.overrideWithValue(supabaseClient),
+        ],
         child: const MaterialApp(home: OnboardingWelcomeScreen()),
       ),
     );
